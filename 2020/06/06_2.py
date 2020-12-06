fd8deb00fb50a29c2e134ecf66809e9a943ef3a4
Andriy Yablonskyy <andriy.yablonskyy@gmail.com>
2020-12-06 09:57:42 -0500

    update vars

--- 2020/06/06_2.py
+++ 2020/06/06_2.py
@@ -1,9 +1,9 @@
-def count_answers(row, target):
-    return sum(row.count(c) == target for c in set(row.replace('\n', '')))
+def count_answers(group, target):
+    return sum(group.count(c) == target for c in set(group.replace('\n', '')))
 
 
 def sum_answers(input_file):
-    return sum(count_answers(r, len(r.split())) for r in open(input_file).read().split('\n\n'))
+    return sum(count_answers(g, len(g.split())) for g in open(input_file).read().split('\n\n'))
 
 
 assert sum_answers('input-06.txt') == 3445
